755bfbb52d1c7b454fcb61ec81193fac96128dfc
mokapsing <39441028+mokapsing@users.noreply.github.com>
2025-09-04 17:40:32 +0800

    Update build.gradle.kts

--- plugin/rime/build.gradle.kts
+++ plugin/rime/build.gradle.kts
@@ -1,56 +1,42 @@
 plugins {
-    id("org.fcitx.fcitx5.android.app-convention")
-    id("org.fcitx.fcitx5.android.plugin-app-convention")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
     id("org.fcitx.fcitx5.android.native-app-convention")
-    id("org.fcitx.fcitx5.android.build-metadata")
-    id("org.fcitx.fcitx5.android.data-descriptor")
-    id("org.fcitx.fcitx5.android.fcitx-component")
 }
 
 android {
     namespace = "org.fcitx.fcitx5.android.plugin.rime"
 
     defaultConfig {
-        applicationId = "org.fcitx.fcitx5.android.plugin.rime"
+        minSdk = 23
+        targetSdk = 34
 
-        @Suppress("UnstableApiUsage")
         externalNativeBuild {
             cmake {
-                targets(
-                    "rime"
-                )
+                targets("rime")  // 确保和 CMakeLists.txt 里的 target 对应
             }
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
         }
     }
 
     buildTypes {
         release {
-            resValue("string", "app_name", "@string/app_name_release")
-        }
-        debug {
-            resValue("string", "app_name", "@string/app_name_debug")
+            minifyEnabled = false
         }
     }
 
     packaging {
         jniLibs {
-            excludes += setOf(
-                "**/libc++_shared.so",
-                "**/libFcitx5*"
-            )
+            excludes += setOf("**/libc++_shared.so")
         }
     }
 }
 
-fcitxComponent {
-    installPrebuiltAssets = true
-}
-
-generateDataDescriptor {
-    symlinks.put("usr/share/rime-data/opencc", "usr/share/opencc")
-}
-
 dependencies {
-    implementation(project(":lib:fcitx5"))
     implementation(project(":lib:plugin-base"))
 }
